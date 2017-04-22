6618618f68bccfb4c6cab1049f1581d263449661
Chris Winsor <cwinsor@gmail.com>
2017-04-22 15:23:48 -0400

    git clone switch

--- bash/scripts/build-basic-app.sh
+++ bash/scripts/build-basic-app.sh
@@ -191,30 +191,29 @@ fi
 
 
 # Checkout the nodejs-starter
-if [ -d "$GIT_FRONT_END_FILENAME" ]; then
-  if [[ $DO_GIT_CLONE -eq 1 ]]; then
+if [[ $DO_GIT_CLONE -eq 1 ]]; then
+  if [ -d "$GIT_FRONT_END_FILENAME" ]; then
     __append_new_line_log "Deleting existing directory \"$GIT_FRONT_END_FILENAME\"..." "$buildBasicAppLogDir"
     if rm -rf "$GIT_FRONT_END_FILENAME"; then
       __append_new_line_log "Successfully deleted!" "$buildBasicAppLogDir"
     else
       __error_exit "There was an error deleting the directory: \"$GIT_FRONT_END_FILENAME\"" "$buildBasicAppLogDir"
     fi
-  
-    getRepoURL "predix-nodejs-starter" GIT_PREDIX_NODEJS_STARTER_URL
-    getRepoVersion "predix-nodejs-starter" GIT_PREDIX_NODEJS_STARTER_VERSION
-    if [ ! -n "$GIT_PREDIX_NODEJS_STARTER_VERSION" ]; then
-      GIT_PREDIX_NODEJS_STARTER_VERSION="$BRANCH"
-    fi
-  
-    if git clone -b "$GIT_PREDIX_NODEJS_STARTER_VERSION" "$GIT_PREDIX_NODEJS_STARTER_URL" "$GIT_FRONT_END_FILENAME"; then
-      cd "$GIT_FRONT_END_FILENAME"
-      __append_new_line_log "Successfully cloned \"$GIT_FRONT_END_FILENAME\" and checkout the branch \"$GIT_PREDIX_NODEJS_STARTER_VERSION\"" "$buildBasicAppLogDir"
-    else
-      __error_exit "There was an error cloning the repo \"$GIT_FRONT_END_FILENAME\". Be sure to have permissions to the repo, or SSH keys created for your account" "$buildBasicAppLogDir"
-    fi
-  else
-    __append_new_line_log "Preserving existing directory \"$GIT_FRONT_END_FILENAME\"..." "$buildBasicAppLogDir"
   fi
+  getRepoURL "predix-nodejs-starter" GIT_PREDIX_NODEJS_STARTER_URL
+  getRepoVersion "predix-nodejs-starter" GIT_PREDIX_NODEJS_STARTER_VERSION
+  if [ ! -n "$GIT_PREDIX_NODEJS_STARTER_VERSION" ]; then
+    GIT_PREDIX_NODEJS_STARTER_VERSION="$BRANCH"
+  fi
+  
+  if git clone -b "$GIT_PREDIX_NODEJS_STARTER_VERSION" "$GIT_PREDIX_NODEJS_STARTER_URL" "$GIT_FRONT_END_FILENAME"; then
+    cd "$GIT_FRONT_END_FILENAME"
+    __append_new_line_log "Successfully cloned \"$GIT_FRONT_END_FILENAME\" and checkout the branch \"$GIT_PREDIX_NODEJS_STARTER_VERSION\"" "$buildBasicAppLogDir"
+  else
+    __error_exit "There was an error cloning the repo \"$GIT_FRONT_END_FILENAME\". Be sure to have permissions to the repo, or SSH keys created for your account" "$buildBasicAppLogDir"
+  fi
+else
+  __append_new_line_log "Preserving existing directory \"$GIT_FRONT_END_FILENAME\"..." "$buildBasicAppLogDir"
 fi
 
 #Checkout the tag if provided by user
